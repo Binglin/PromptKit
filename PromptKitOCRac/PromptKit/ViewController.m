d9742e6b955fc4edb31d305461f1d101bf3641e0
Binglin <binglin@uoko.com>
2016-11-28 17:47:48 +0800

    prepare to write example

--- PromptKitOCRac/PromptKit/ViewController.m
+++ PromptKitOCRac/PromptKit/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "PromptKit.h"
 
 @interface ViewController ()
 
@@ -22,6 +23,57 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (void)manageLoading{
+    
+    RACSignal *signal = [[RACSignal return:@(1)] delay:3];
+    
+    id<PKLoadingViewProtocol> del = self;
+    
+    [[signal pk_addLoading:del] subscribeNext:^(id x) {
+        // next of signal
+        
+    }];
+}
+
+- (void)manageEmpty{
+    
+    PKPromptUIDataSource *empty = [[PKPromptUIDataSource alloc] initWithTitle:@"emptyTitle" logo:@"empty_logo_name"];
+    
+    RACSignal *signal = [RACSignal empty];
+    
+    id<PKEmptyViewProtocol> del = self;
+    
+    [[signal pk_observerEmpty:del emptyData:empty] subscribeNext:^(id x) {
+        // next of signal
+    }];
+}
+
+
+- (void)manageToast{
+    RACSignal *signal = [RACSignal return:[NSError errorWithDomain:@"domin" code:-1 userInfo:@{NSLocalizedDescriptionKey: @"toast error description"}]];
+    
+    id<PKErrorToastProtocol> del = self;
+    
+    [[signal pk_observerErrorToast:del] subscribeNext:^(id x) {
+        // next of signal
+    }];
+}
+
+- (void)manageError{
+    
+    RACSignal *signal = [RACSignal empty];
+    
+    id<PKErrorViewProtocol> del = self;
+    
+    [[signal pk_observerError:del reload:^{
+        // action execute by tap button or view to reload when error occure
+        
+    }] subscribeNext:^(id x) {
+        // next of signal
+    }];
 }
 
 @end
